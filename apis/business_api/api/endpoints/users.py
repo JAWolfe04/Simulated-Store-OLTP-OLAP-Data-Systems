5d2d7129022d4b6d42bab8ad0a4af96c3e8167ca
Jonathan <jawhf4@mail.umkc.edu>
2021-07-07 01:19:17 -0500

    Added remove user and removed read users endpoints
    - Added remove user endpoint to allow a user to remove their account
    - Changed create user endpoint to collect form data
    - Removed read users end point

--- apis/business_api/api/endpoints/users.py
+++ apis/business_api/api/endpoints/users.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Form
 from sqlalchemy.orm import Session
 
 from apis.business_api import crud, models, schemas
@@ -8,15 +8,22 @@ from apis.business_api.api import dependencies as deps
 
 router = APIRouter()
 
-@router.get("/", response_model = List[schemas.User])
-def read_users(db: Session = Depends(deps.get_db)):
-    users = crud.user.get_users(db)
-    return users
-
 @router.post("/", response_model = schemas.User)
-def create_user(user: schemas.UserCreate,
-                db: Session = Depends(deps.get_db)):
-    db_user = crud.user.get_user(db, username=user.username)
+def create_user(db: Session = Depends(deps.get_db),
+                username: str = Form(...),
+                password: str = Form(...)):
+    db_user = crud.user.get_user(db, username = username)
     if db_user:
         raise HTTPException(status_code=400, detail="Username already exists")
+    user = schemas.UserCreate(username = username, password = password)
     return crud.user.create_user(db = db, user = user)
+
+@router.delete("/")
+def delete_user(db: Session = Depends(deps.get_db),
+                username: str = Form(...),
+                password: str = Form(...)):
+    user = crud.user.authenticate(db, username = username, password = password)
+    if not user:
+        raise HTTPException(status_code = 400,
+                            detail = "Invalid username or password")
+    crud.user.delete_user(db = db, user = user)
--- apis/business_api/crud/user.py
+++ apis/business_api/crud/user.py
@@ -8,9 +8,6 @@ class Crud_User:
     def get_user(self, db: Session, username: str):
         return db.query(models.User).filter(models.User.username == username).first()
 
-    def get_users(self, db: Session):
-        return db.query(models.User).all()
-
     def create_user(self, db: Session, user: schemas.UserCreate):
         db_user = models.User(
             username = user.username,
@@ -28,5 +25,9 @@ class Crud_User:
         if not security.verify_password(password, user.hash_password):
             return False
         return user
+
+    def delete_user(self, db: Session, user: schemas.UserCreate):
+        db.delete(user)
+        db.commit()
 
 user = Crud_User()
